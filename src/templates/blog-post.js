1e3d26d1b3399644767ae1afdc034e8dac8d88fa
Fakhruddin Ahmad Darwis <fakhri.satu@gmail.com>
2020-09-21 20:56:42 +0700

    Changed similar posts header

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -61,7 +61,7 @@ class ArticleTemplate extends Component {
           <div>
             {similarPosts.length > 0 && (
               <h3 id="similar-posts-header">
-                Other {this.props.pageContext.topic} Tutorials
+                Artikel {this.props.pageContext.topic} Lainnya
               </h3>
             )}
 
